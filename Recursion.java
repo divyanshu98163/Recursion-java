c8550c7308f7d727e0c60349a38400e6303f7977
divyanshu98163 <divy9anshu>
2024-06-20 16:49:14 +0530

    print decresing order using recursion

---
+++ Recursion.java
@@ -0,0 +1,17 @@
+public class Recursion {
+    
+    public static void PrintDec(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n+"");
+        PrintDec(n-1);
+
+    }
+    public static void main(String[] args) {
+        int n=10;
+        PrintDec(n);
+
+    }
+}
