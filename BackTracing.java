fd9eb1b0a38623c14655fc20b746e8afc1d3978a
divyanshu98163 <divy9anshu>
2024-07-01 10:22:25 +0530

    Back tracing using recursion

---
+++ BackTracing.java
@@ -0,0 +1,28 @@
+public class BackTracing {
+
+
+    public static void changeArr(int arr[],int i,int val){
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+        arr[i] = val; 
+        changeArr(arr, i+1, val+1);
+        arr[i] = arr[i] -2;
+
+    }
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.err.print(arr[i]+" ");
+        }
+        System.err.println();
+    }
+    public static void main(String args[]){
+        int[] arr = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+
+
+    }
+}
